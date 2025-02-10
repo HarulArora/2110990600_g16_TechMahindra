cf599b0170d88fd39818f5c5aac1b7839f674081
Harul Arora <harularora463@gmail.com>
2025-02-10 16:48:32 +0530

    Lecture 4

---
+++ Lec4_100225/script.js
@@ -0,0 +1,25 @@
+// console.log("Hello");
+
+// var a = 10;
+
+// function squareOfTwoNumber(x) {
+//     var ans = x * x;
+//     return ans;
+// }
+
+// console.log(squareOfTwoNumber(a));
+
+
+
+console.log(abc);
+sumoftwo(10, 5);
+var abc = 29;
+
+
+function sumoftwo(a, b) {
+    var ans = a + b;
+    return ans;
+}
+
+var majorAns = sumoftwo(6, 8);
+console.log(major980Ans);
